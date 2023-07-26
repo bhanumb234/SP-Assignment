15e19cdc59551c54a77c9496104214f44070afe0
M BHANU UDAY <59698025+bhanumb234@users.noreply.github.com>
2023-07-26 22:37:25 +0530

    Create file_sorting.sh

---
+++ file_sorting.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#prompt the name of directory
+
+echo "Please enter the name of directory :
+read direct_name
+
+echo "you entered $direct_name"
+
+if I-d "$direct_name"]
+  then
+    echo "Error: Directory '$direct_name' doesn't exists"
+    exit 1
+fi
+
+echo -e "\n $direct_name directory exists"
+
+#List all the names in the given directory
+
+echo "List all the files in $direct_name directory are : "
+Is "$direct_name"
+
+#Sort all the files alphabetically
+
+echo -e "Files in sorted order are : "
+Is $direct_name
+
+neu_direct-$direct_name/sorted
+mkdir -p "$new_direct"
+
+count=0
+for file in "$direct_name"/*
+  do
+    if [ -f "$file" ]
+    then
+      mv "$file" "$new_direct"
+      ((count++))
+    fi
+  done
+echo -e "\n Success: $count files moved from Bhanu to Sorted directory."
